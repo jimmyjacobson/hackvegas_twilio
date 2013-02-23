757e34404e4f1cc295270330c3c3634774281a6a
Jimmy Jacobson <jimmy@wedgies.com>
2013-02-23 06:29:08 -0800

    twiML example

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+
+  var twiML = "<Response>"
+    		+	"<Say>Joining a conference room</Say>"
+    		+	"<Dial>"
+        	+ 		"<Conference>MyRoom</Conference>"
+    		+	"</Dial>"
+			+"</Response>";
+
+
+  res.end(twiML);
 }).listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
 
